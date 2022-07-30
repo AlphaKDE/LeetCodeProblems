2189118cfdaae23a01dda0692dc02765b859965d
AlphaKDE <99363062+AlphaKDE@users.noreply.github.com>
2022-07-30 01:16:08 -0400

    Time: 90 ms (9.89%), Space: 14.2 MB (26.41%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        
+       
+        index = len(nums)
+      
+        for i in range(1,index):
+            nums[i] = nums[i] + nums[i-1]
+                           
+            
+        return nums
+            
+                
+            
+               
+        
+     
+     
+            
