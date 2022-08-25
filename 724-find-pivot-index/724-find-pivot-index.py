7ac858d76ca74ac7c49af51037e861ea8517881b
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-08-25 17:19:14 -0400

    Time: 281 ms (31.16%), Space: 15.2 MB (48.84%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,19 +1,20 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         #we want to work with index.. use enumare to loop
-        S = sum(nums)
-        leftsum = 0 
-        for index,data in enumerate(nums):
-            if leftsum == (S-leftsum - data):
-                return index
-            leftsum += data
-            
-        return -1
-               
-            
-                
-                
-                
+            Sum = sum(nums) # before we loop our right sum is equal to the all the numbers in the array
+            right_sum = Sum 
+            left_sum = 0 # our left sum is zero 
+
+            for i in range(len(nums)):
+                currnum = nums[i] # current number we are at in for loop
+                right_sum  = right_sum - currnum
+                if left_sum == right_sum:
+                    return i
+                left_sum +=currnum
+            return -1    
+
+
+
             
                 
                 
