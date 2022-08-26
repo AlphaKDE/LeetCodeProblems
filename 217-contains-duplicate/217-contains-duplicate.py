6d172a52e88759203231242ffe8f99fcdcce1e70
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-08-26 19:36:52 -0400

    Time: 507 ms (82.76%), Space: 25.6 MB (98.40%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,12 +1,30 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashmap = Counter(nums)
+    
+            
         
-        print(hashmap.values())
-        for occ in hashmap.values():
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        hashmap = Counter(nums) # we set up a counter that counts the occ of the values in nums 
+        
+        print(hashmap.values()) # testing to see if it outputs the correct values 
+        for occ in hashmap.values(): # looping over the values
             if occ >= 2:
                 return True
         return False
             
+#         #time complexity: O(n) - where n is the numbers in the array
+#         #space complexity: O(n) - we are creating a new data structure to store the occurances of the n
             
+        
+        
+        
         
