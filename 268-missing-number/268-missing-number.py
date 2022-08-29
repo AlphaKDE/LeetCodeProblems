d32241590e4d2a67f414fa3304a32cc38b340fe1
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-08-29 13:46:24 -0400

    Time: 336 ms (16.99%), Space: 15.2 MB (38.15%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,19 +1,28 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
+        
+        res = len(nums)
+        
+        for i in range(len(nums)):
+            res = res ^ i ^ nums[i]
+        return res
+        
+        
         #1. if we are given [0,n] the amount of numbers in the list will be n+1
-        correct_range = len(nums)
-        correct_sets = set([i for i in range(correct_range+1)]) # creating a hasmap to store the correct values given a range 
+#         correct_range = len(nums)
+#         correct_sets = set([i for i in range(correct_range+1)]) # creating a hashset to store the correct values given a range 
         
         
                 
-        print(correct_sets)
+#         print(correct_sets)
         
         
-        for item in correct_sets:
-            if item not in nums:
-                return item
-        
+#         for item in correct_sets:
+#             if item not in nums:
+#                 return item
+#         #time:O(n)
+#         #space :O(n)
            
            
         
