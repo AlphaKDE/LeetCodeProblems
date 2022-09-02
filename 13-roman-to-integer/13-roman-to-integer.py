31adb58f36a0d53f4294e5e91488c6fccc10e40b
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-09-02 02:13:27 -0400

    Time: 111 ms (7.31%), Space: 14.1 MB (30.15%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -4,13 +4,19 @@ class Solution:
         roman = {"I":1, "V": 5, "X": 10, "L": 50, "C":100,
                 "D": 500, "M": 1000}
         result = 0 
+        print(len(s))
         
         for item in range(len(s)):
-            if item  + 1 <len(s) and roman[s[item]] < roman[s[item + 1]]:
+            print(item)
+            if item + 1 < len(s) and roman[s[item]]< roman[s[item+1]]:
                 result -= roman[s[item]]
             else:
-                result+= roman[s[item]]
+                result += roman[s[item]]
+                
+                
         return result
+        
+     
             
                 
 
