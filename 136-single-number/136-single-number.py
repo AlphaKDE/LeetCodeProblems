b509950e7ffd2b8b3af04eb5bb09dce88c5effed
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-09-03 15:53:55 -0400

    Time: 210 ms (57.30%), Space: 17.3 MB (5.17%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        sequence = Counter(nums)
+        
+        print(sequence)
+        
+        for count in sequence:
+            if sequence[count] == 1:
+                return count
+          
+           
+        
