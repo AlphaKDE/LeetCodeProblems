6be800bd4f47b9e723def805070db1ac831f2ad1
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-09-04 19:29:24 -0400

    Time: 68 ms (90.96%), Space: 15.1 MB (49.66%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,26 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #looping through the nums 
-        map = {}
+        
+        
+#         #O(n^2) time 
+#         #0(1) space
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)): # starting at i+1 to not repeat two of the same values.
+#                 print(nums[i],nums[j])
+#                 if nums[j] == target - nums[i]:
+                    
+#                     return [i,j]
+          #O(n) time O(n) space  
+        map = {} # initilizing an empty map 
         
         for index, value in enumerate(nums):
-            diff = target - value
+            diff = target - value #
             if diff in map:
                 return [map[diff],index]
             map[value] = index
+        
+        
             
     
     
