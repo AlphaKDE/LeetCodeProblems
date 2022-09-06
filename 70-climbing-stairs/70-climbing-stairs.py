6ae469e1a983a7281d51c9094a5a07e0cd8d9ced
Keny Exantus <99363062+AlphaKDE@users.noreply.github.com>
2022-09-06 18:49:54 -0400

    Time: 28 ms (96.75%), Space: 13.8 MB (56.84%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,5 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # if n == 1 or n == 2:
+        #     return n
+        # return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        
         path = {1:1, 2:2, 3:3}
         
         for i in range(4, n+1):
